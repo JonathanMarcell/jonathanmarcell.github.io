02ab057504d8d45f2db15a5ed2d8fc8013c80ce3
jonathan <jonathanmarcellatmadja.jma@gmail.com>
2024-08-02 16:58:35 +0700

    forgot to build

---
+++ _app/immutable/chunks/global.Bqx5j-g7.js
@@ -0,0 +1,1 @@
+const o="#060D0C",c="#E9EFF2",s="#48D995",_="#50F296",t="#025940",a="#094036";export{c as a,o as b,_ as c,t as d,a as e,s as f};
---
+++ _app/immutable/entry/start.BGwhVgBB.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bc8ECiGo.js";export{t as start};
